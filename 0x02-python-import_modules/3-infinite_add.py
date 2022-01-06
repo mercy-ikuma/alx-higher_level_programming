8c2dc4b36573e5a0c7ec077ce6e0428459c696fa
mercy-ikuma <84702006+mercy-ikuma@users.noreply.github.com>
2022-01-06 13:26:32 +0300

    python

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
+
+if __name__ == "__main__":
+    import sys
+    add_arg(sys.argv)
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
